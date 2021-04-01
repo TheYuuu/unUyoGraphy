ba3982362530f69575e027f841d5a2581afdd327
luoyu11 <luoyu11@meituan.com>
2021-04-01 21:13:30 +0800

    两轴热力完成

--- packages/AxisHeadMap/sample.ts
+++ packages/AxisHeadMap/sample.ts
@@ -1,13 +1,15 @@
 export const xPos = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'];
+  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 export const yPos = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1',
-  'H1', 'I1', 'J1', 'K1', 'L1', 'M1', 'N1', 'O1', 'P1', 'Q1', 'R1', 'S1', 'T1'];
+  'H1', 'I1', 'J1', 'K1', 'L1', 'M1', 'N1', 'O1', 'P1', 'Q1', 'R1',
+    'S1', 'T1', 'U1', 'V1', 'W1', 'X1', 'Y1', 'Z1'];
 
 const data = new Array(300).fill(0).map(() => {
   return {
     value: Number((Math.random() * 100).toFixed(0)),
-    xPos: xPos[Number((Math.random() * 19).toFixed(0))],
-    yPos: yPos[Number((Math.random() * 19).toFixed(0))]
+    xPos: xPos[Number((Math.random() * (xPos.length - 1)).toFixed(0))],
+    yPos: yPos[Number((Math.random() * (yPos.length - 1)).toFixed(0))]
   }
 });
 
