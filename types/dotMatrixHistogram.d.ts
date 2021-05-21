409e15b1b808ea94ee1201691aaca6920cfe607b
TheYu <luoyu.creative@icloud.com>
2021-05-21 11:32:34 +0800

    type

--- types/dotMatrixHistogram.d.ts
+++ types/dotMatrixHistogram.d.ts
@@ -1,16 +1,41 @@
 import ChartBase, { defaultOptions, defaultOpts } from './chartBase';
-
-export interface opts extends defaultOpts {}
-
+export interface opts extends defaultOpts {
+    bars?: number;
+    barPadding?: number;
+    colors?: string[];
+}
+export interface dotMatrixHistogramDotData {
+    seriesType: string;
+}
+export interface dotMatrixHistogramData {
+    seriesX: string;
+    data: dotMatrixHistogramDotData[];
+}
+export interface dotMatrixHistogramOptionData {
+    data: dotMatrixHistogramData[];
+    seriesX: string[];
+    seriesTypes: string[];
+}
 export interface dotMatrixHistogramOptions extends defaultOptions {
-    data: dotMatrixHistogramData;
+    data: dotMatrixHistogramOptionData;
     opts?: opts;
 }
-
 export default class DotMatrixHistogram extends ChartBase {
-    private data;
     private opts;
+    private data;
+    private yAxis_g;
+    private xAxis_g;
+    private dot_g;
+    private xPosAxis;
+    private colorMap;
+    private dotWidth;
+    private maxHieght;
     constructor(opt: dotMatrixHistogramOptions);
+    private init;
     update(data: dotMatrixHistogramOptionData): void;
+    private handleDate;
+    private draw;
+    private updateDots;
+    private updateYAxis;
+    private updateXAxis;
 }
-  
